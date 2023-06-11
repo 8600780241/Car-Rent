69ffae209d779421aedf25defb55500381ae3ccb
hiteshnavghade75 <hiteshnavghade75@gmail.com>
2023-06-11 14:54:47 +0530

    bugs fixed

--- Client/rentalcar/src/components/Landing.js
+++ Client/rentalcar/src/components/Landing.js
@@ -2,38 +2,50 @@ import React, { useState } from 'react';
 import { Outlet, Link } from "react-router-dom";
 import image from './backImage.png'
 export default function Landing() {
-  
-    function signInHandler(e) {
-        e.preventDefault();
-        const error = validate();
-        if (error) {
-            setErrorData(error);
-        } else {
-            setErrorData();
-        }
+    const [user,setUser] = useState("");
+    const [password,setPassword] = useState("");
+    const [userError,setUserError] = useState(false);
+    const [userPassword,setUserPassword] = useState(false);
+ function signInHandler(e) {
+    e.preventDefault();
+    // console.log(user)
+    // console.log(password)
+ }
+ function userHandler(e) {
+    let text = e.target.value;
+    if(text.includes("@") || text.length > 3) {
+        setUserError(false);
+    } else {
+        setUserError(true)
     }
+    setUser(text)
+ }
+
+ function passwordHandler(e) {
+    let text = e.target.value;
+    if(text.includes("#") || text.includes("@") || text.length > 6) {
+        setUserPassword(false)
+    } else {
+        setUserPassword(true)
+    }
+    setPassword(text)
+ }
     return (<div className='landing'>
         <div id='logo'>Logo</div>
         <div className='register' style={{ backgroundImage: `url(${image})` }}>
             <div className='signInPart1'>
                 <div className='textPart1'>All you needed was a wheel in Your hand and four on the road.
                     <div id='route'>
-
-                        <Link to="/userRegister"><div id='rRoute'>Register</div></Link>
-                        <Link to="/admin/signIn"><div id='aRoute'>Admin Login</div></Link>
-                    </div>
-
                        <Link to="/userRegister"><div id='rRoute'>Register</div></Link> 
                        <Link to="/admin/signIn" ><div id='aRoute'>Admin Login</div></Link> 
-                    </div> 
-
+                    </div>
+                 
                 </div>
                 <div className='blockPart1'>
                     <p>Sign in your Account</p>
                     <form onSubmit={signInHandler}>
-                        {errorData && <div>{errorData}</div>}
-                        <input type='text' className='input1' placeholder=' Email' onChange={e => setFormData(data => ({ ...formData, email: e.target.value }))} value={formData.email} />
-                        <input type='password' className='input1' placeholder='  Password' onChange={e => setFormData(data => ({ ...formData, password: e.target.value }))} value={formData.password} />
+                        <input type='text' className='input1' placeholder='  Phone/Email' onChange={userHandler} value={user}/>{userError?<span>atleast 4 letters</span>:""}<br></br>
+                        <input type='password' className='input1' placeholder='  Password' onChange={passwordHandler} value={password}/>{userPassword?<span>not valid</span>:""}
                         <p id='forgetPassword'>Forget Password?</p>
                         <button className='signInbutton' type='submit'>SIGN IN</button>
                         <Link to="/userRegister"><p id='createAccount'>Create Account</p></Link>
--- Client/rentalcar/src/components/UserRegister.js
+++ Client/rentalcar/src/components/UserRegister.js
@@ -93,4 +93,4 @@ export default function AdminRegister() {
     </div>
   )
 }
-}
+
